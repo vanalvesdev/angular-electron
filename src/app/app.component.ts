822c7a9dfe129bfba05ae08d66f27904f5a54736
Vanderson Alves <vanalvesdev@gmail.com>
2020-09-06 17:33:56 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs'
+import { takeWhile, tap } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-electron';
+  max = 1;
+  current = 0;
+
+  start() {
+    const inter = interval(100);
+
+    inter.pipe(
+      takeWhile(val => !this.isFinished),
+      tap(i => this.current += 0.1)
+    ).subscribe();
+  }
+
+  finish() {
+    this.current = this.max;
+  }
+
+  reset() {
+    this.current = 0;
+  }
+
+
+  // Getters to prevent NaN errors
+  get maxVal() {
+    return isNaN(this.max) || this.max < 0.1 ? 0.1 : this.max;
+  }
+
+  get currentVal() {
+    return isNaN(this.current) || this.current < 0 ? 0 : this.current;
+  }
+
+  get isFinished() {
+    return this.currentVal >= this.maxVal;
+  }
 }
